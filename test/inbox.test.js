d9f9c2caec6200cc6d495133300b4bae5458daef
bwakedavis <bwakedavis@gmail.com>
2021-01-27 12:40:07 +0300

    written 3 tests with mocha

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,7 +1,9 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const  web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+const  web3 = new Web3(provider);
 const {interface,bytecode} = require('../compile')
 let accounts;
 let inbox;
@@ -18,7 +20,17 @@ inbox = await new web3.eth.Contract(JSON.parse(interface)).deploy({data:bytecode
 
 describe('Inbox',()=>{
     it('it deploys a contract',()=>{
-        console.log(inbox)
+        assert.ok(inbox.options.address)
+    })
+
+    it('has a default message', async ()=>{
+        const message = await inbox.methods.message().call();
+        assert.strictEqual(message,'Hi There!');
+    })
+    it('can change the message', async ()=>{
+        await inbox.methods.setMessage('Bye Broh!').send({from: accounts[0]})
+        const message = await inbox.methods.message().call();
+        assert.strictEqual(message,'Bye Broh!');
     })
 })
 
